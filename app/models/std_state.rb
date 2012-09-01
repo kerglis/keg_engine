010a82763065efe8c39d8e43b5bec0dfd089a1e1
Kristaps Erglis <kristaps.erglis@tieto.com>
2012-09-01 14:40:54 +0300

    StdState

---
+++ app/models/std_state.rb
@@ -0,0 +1,30 @@
+module StdState
+
+  def self.included(base)
+    base.class_eval do
+
+      scope :active,  where(:state => :active)
+
+      state_machine :initial => :inactive do
+
+        event :activate do
+          transition :to => :active, :from => :inactive, :if => :valid_for_activation?
+        end
+
+        event :deactivate do
+          transition :to => :inactive, :from => :active
+        end
+
+        event :swap do
+          transition  :active => :inactive, :inactive => :active, :if => lambda {|obj| obj.active? || obj.valid_for_activation? }
+        end
+      end
+
+      def valid_for_activation?
+        valid?
+      end
+
+    end
+  end
+
+end
