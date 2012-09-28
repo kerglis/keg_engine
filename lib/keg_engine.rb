d1b017894fde81a7eca3d9b5a15328f88892ece4
Kristaps Erglis <kristaps.erglis@tieto.com>
2012-09-28 11:43:52 +0300

    require 'preferences'

--- lib/keg_engine.rb
+++ lib/keg_engine.rb
@@ -1,12 +1,13 @@
-require "devise"
-require "config_spartan"
-require "acts_as_list"
-require "simple_form"
-require "state_machine"
-require "will_paginate"
-require "inherited_resources"
-require "meta_search"
-require "keg_engine/engine"
+require 'devise'
+require 'config_spartan'
+require 'acts_as_list'
+require 'simple_form'
+require 'state_machine'
+require 'will_paginate'
+require 'inherited_resources'
+require 'meta_search'
+require 'preferences'
+require 'keg_engine/engine'
 
 module KegEngine
 end
