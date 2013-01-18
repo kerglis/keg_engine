7c5025ca94624777d0ab59aa79ca495cf7f0aa74
Kristaps Erglis <kristaps.erglis@gmail.com>
2013-01-18 09:50:06 +0200

    flags + dim

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   # attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_protected  :admin
 
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :registerable, :recoverable, :trackable, :validatable, :omniauthable
 
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible   :email, :first_name, :last_name, :password, :password_confirmation
+  attr_accessible   :admin, :gender
 
   def self.find_for_facebook_oauth(access_token, signed_in_resource = nil)
     data = access_token.extra.raw_info
