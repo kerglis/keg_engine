cb6df3ac599828df31e4ae4a642c06d8b7674cb0
Kristaps Erglis <kristaps.erglis@gmail.com>
2013-02-11 20:03:23 +0200

    SetUser fix

--- app/models/set_user.rb
+++ app/models/set_user.rb
@@ -2,7 +2,7 @@ module SetUser
 
   def self.included(base)
     base.instance_eval do
-      validates_presence_of :user_id
+      validates_presence_of :user
       before_validation :set_user
       belongs_to :user
       attr_accessible :user, :user_id
@@ -12,7 +12,7 @@ module SetUser
   private
 
   def set_user
-    self.user_id ||= User.current.try(:id)
+    self.user ||= User.current
   end
 
 end
